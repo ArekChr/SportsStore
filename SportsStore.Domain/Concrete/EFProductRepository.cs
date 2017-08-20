a860a6406656c2d9992d768ef90ab761625451eb
Aras <arkadiuszchr@gmail.com>
2017-08-20 14:35:13 +0200

    Dodaj pliki projektów.

---
+++ SportsStore.Domain/Concrete/EFProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+
+namespace SportsStore.Domain.Concrete
+{
+    public class EFProductRepository : IProductRepository
+    {
+        private EFDdContext context = new EFDdContext();
+
+        public IEnumerable<Product> Products
+        {
+            get { return context.Products; }
+        }
+    }
+}
---
+++ SportsStore.UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SportsStore.UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SportsStore.UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c0e0cf5d-a467-43bb-87bc-c3de7719cfd3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
