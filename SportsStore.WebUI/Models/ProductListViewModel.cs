5b220863c6db47a6223f0c91bfdbd667e2d5507f
Aras <arkadiuszchr@gmail.com>
2017-08-20 17:53:58 +0200

    Add Category

--- SportsStore.WebUI/Models/ProductListViewModel.cs
+++ SportsStore.WebUI/Models/ProductListViewModel.cs
@@ -5,8 +5,8 @@ namespace SportsStore.WebUI.Models
 {
     public class ProductListViewModel
     {
-         public IEnumerable<Product> Products { get; set; }
-         public PagingInfo PagingInfo { get; set; }
-        
+        public IEnumerable<Product> Products { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+        public string CurrentCategory { get; set; }
     }
 }
