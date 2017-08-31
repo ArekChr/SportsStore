aa5463ba794151439cb30ebcbcbca6d30ea3e5d0
Aras <arkadiuszchr@gmail.com>
2017-08-31 21:08:21 +0200

    Account Login functions

--- SportsStore.WebUI/Infrastructure/NinjectDependenctResolver.cs
+++ SportsStore.WebUI/Infrastructure/NinjectDependenctResolver.cs
@@ -8,6 +8,8 @@ using Ninject;
 using SportsStore.Domain.Abstract;
 using SportsStore.Domain.Entities;
 using SportsStore.Domain.Concrete;
+using SportsStore.WebUI.Infrastructure.Abstract;
+using SportsStore.WebUI.Infrastructure.Concrete;
 
 namespace SportsStore.WebUI.Infrastructure
 {
@@ -36,6 +38,8 @@ namespace SportsStore.WebUI.Infrastructure
             };
 
             kernel.Bind<IOrderProcessor>().To<EmailOrderProcessor>().WithConstructorArgument("settings", emailSettings);
+
+            kernel.Bind<IAuthProvider>().To<FormsAuthProvider>();
 
         }
     }
